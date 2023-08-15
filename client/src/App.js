58847d41018b8709068869cdf262c0ec591b2fe5
chakrinya <chakrinyabalabadra@gmail.com>
2023-08-16 01:36:20 +0530

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
-import Headers from "./components/header/Headers";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, NotFound } from "./Components/default";
+import { Box } from "@mui/material";
 
-const App = () => {
+//components
+import Header from "./Components/Header/Header";
+import DetailView from "./Components/ItemDetails/DetailView";
+import TemplateProvider from "./templates/TemplateProvider";
+import ContextProvider from "./context/ContextProvider";
+import Cart from "./Components/Cart/Cart";
+
+function App() {
   return (
-    <div>
-      <Headers />
-    </div>
+    <TemplateProvider>
+      <ContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Box style={{ marginTop: 54 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/product/:id" element={<DetailView />} />
+            </Routes>
+          </Box>
+        </BrowserRouter>
+      </ContextProvider>
+    </TemplateProvider>
   );
-};
+}
 
 export default App;
